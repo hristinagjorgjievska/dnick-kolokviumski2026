81fdb9ded51382503fa70a19f093f0cf39b523d7
hristinagjorgjievska <hristinna791@gmail.com>
2026-05-29 15:04:02 +0200

    model change and admin

--- kolokviumska-1/myproject/myapp/admin.py
+++ kolokviumska-1/myproject/myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname", "phone", "email"]
+
+class CakeAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "weight"]
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.user == request.user:
+            return True
+        return False
+
