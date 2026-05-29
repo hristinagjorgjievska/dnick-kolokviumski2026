d42d1345a3b02bd59da45cb2ebd8ad7367c55262
hristinagjorgjievska <hristinna791@gmail.com>
2026-05-29 15:04:16 +0200

    admin register

--- kolokviumska-1/myproject/myapp/admin.py
+++ kolokviumska-1/myproject/myapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["name", "surname", "phone", "email"]
 
+@admin.register(Cake)
 class CakeAdmin(admin.ModelAdmin):
     list_display = ["name", "price", "weight"]
 
