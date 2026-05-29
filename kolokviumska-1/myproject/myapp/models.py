f12d0d2a62eceab8675bf57f19777ef7794f965a
hristinagjorgjievska <hristinna791@gmail.com>
2026-05-29 14:49:30 +0200

    add models

--- kolokviumska-1/myproject/myapp/models.py
+++ kolokviumska-1/myproject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Cake(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField()
