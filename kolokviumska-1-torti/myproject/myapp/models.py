2e91ed6ba20447716853b298aba69728cd8491f1
hristinagjorgjievska <hristinna791@gmail.com>
2026-05-31 00:21:14 +0200

    add changes

--- kolokviumska-1-torti/myproject/myapp/models.py
+++ kolokviumska-1-torti/myproject/myapp/models.py
@@ -1,22 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Baker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
 
 class Cake(models.Model):
-    name = models.CharField(max_length=20)
-    #Овде on_delete=models.SET_NULL поради условот ,,Кога се брише пекарот, неговите торти по случаен избор се додаваат на останатите пекари''
+    name = models.CharField(max_length=100)
     baker = models.ForeignKey(Baker, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     weight = models.PositiveIntegerField()
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
 
-
+    def __str__(self):
+        return self.name
--- kolokviumska-torti/myproject/myapp/admin.py
+++ kolokviumska-torti/myproject/myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+@admin.register(Baker)
+class BakerAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname", "phone", "email"]
+
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    list_display = ["name", "baker", "price", "weight"]
+
